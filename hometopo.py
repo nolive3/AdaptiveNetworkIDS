d16e3c08e08feb10ca082f20a14ea1600c2b1a93
Nick Olive <nolive3@gatech.edu>
2014-04-03 23:52:53 -0700

    Configurable home network topology python code
    Python link for any needed net, command line configs to come soon.
    
    to configure set the names arguments desired in the constructor
    options are:
    	ethHosts- the number of hosts on the virtual switch
    	wifiHosts- the number of hosts connected on wifi
    	hostConfig- set the host config for all machines
    	internetLinkConfig- set the link config for the internet
    	ethernetLinkConfig- set the config for links simulating a single
    			ethernet cable
    	wifiLinkConfig- set the link configs for all links simulating a
    			wireless connection

---
+++ hometopo.py
@@ -0,0 +1,56 @@
+from mininet.net import Mininet
+from mininet.topo import Topo
+from mininet.node import CPULimitedHost
+from mininet.link import TCLink
+from mininet.util import dumpNodeConnections
+from mininet.cli import CLI
+
+# A typical home network
+class HomeTopo(Topo):
+    "Mininet test topology"
+
+    def __init__(self, ethHosts = 1, wifiHosts = 0, hostConfig = {'cpu': 0.1}, internetLinkConfig = {'bw': 10, 'delay': '10ms', 'loss': 0, 'max_queue_size': None }, wifiLinkConfig = {'bw': 1, 'delay': '5ms', 'loss': 10, 'max_queue_size': None }, ethernetLinkConfig = {'bw': 100, 'delay': '1ms', 'loss': 0, 'max_queue_size': None }, **params):
+        Topo.__init__(self, **params)
+        self.internet = self.addSwitch('s0')
+        self.router = self.addSwitch('s1')
+        self.switch = self.addSwitch('s2')
+        self.wifi = self.addSwitch('s3')
+
+        self.addLink(self.router, self.switch, **ethernetLinkConfig)
+        self.addLink(self.wifi, self.switch, **ethernetLinkConfig)
+        self.addLink(self.router, self.internet, **internetLinkConfig)
+
+        self.ethHosts = []
+        self.wifiHosts = []
+
+
+        self.remote = self.addHost('remote', **hostConfig)
+        self.evil = self.addHost('evil', **hostConfig)
+
+        for i in range(ethHosts):
+            self.ethHosts.append(self.addHost('lan'+str(i), **hostConfig))
+        for i in range(wifiHosts):
+            self.wifiHosts.append(self.addHost('wlan'+str(i), **hostConfig))
+
+        self.addLink(self.remote, self.internet, **internetLinkConfig)
+        self.addLink(self.evil, self.internet, **internetLinkConfig)
+
+        for host in self.ethHosts:
+            self.addLink(self.switch, host, **ethernetLinkConfig)
+        for host in self.wifiHosts:
+            self.addLink(self.wifi, host, **wifiLinkConfig)
+
+
+
+
+def main():
+    topo = HomeTopo()
+    net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink,
+                  autoPinCpus=True)
+    net.start()
+    dumpNodeConnections(net.hosts)
+    net.pingAll()
+    CLI( net )    
+
+if __name__ == '__main__':
+    main()
