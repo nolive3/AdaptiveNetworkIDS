4094b4a4407e269136dbb68ee3c6da912eb4e6b0
David Emmel <david.emmel.g@gmail.com>
2014-04-15 15:53:14 -0400

    Revert "Add PyResonance"
    This reverts commit 1c462256cd9ad489e8f8758532680ea5afdd90bf.
    
    Conflicts:
    	pyretic

--- pyretic/mininet.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo ~/pyretic/mininet/mn -c
-sudo ~/pyretic/mininet/mn --custom $HOME/pyretic/mininet/extra-topos.py --controller remote --mac $@
--- pyretic/start.sh
+++
@@ -1,1 +0,0 @@
-pyretic.py pyretic.pyresonance.main --config=./pyretic/pyresonance/global.config --mode=manual
