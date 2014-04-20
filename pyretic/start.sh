e1451aefdf5c6eaa429bd65fa393c31af1fd4867
David Emmel <dgemmel2@gmail.com>
2014-04-20 21:51:33 +0000

    Added proactive mode and got rid of stupid junk.

--- pyretic/start.sh
+++ pyretic/start.sh
@@ -1,1 +1,1 @@
-./pyretic.py pyretic.pyresonance.main --config=./pyretic/pyresonance/global.config --mode=manual
+./pyretic.py -m p0 pyretic.pyresonance.main --config=./pyretic/pyresonance/global.config --mode=manual
